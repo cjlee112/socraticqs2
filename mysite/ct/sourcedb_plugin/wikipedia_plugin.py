0cf8ba88663e6e5b48c82bc07c3a955b96a547a4
Christopher Lee <leec@chem.ucla.edu>
2014-08-21 13:05:38 -0700

    added wikipedia plugin missing from repo

---
+++ mysite/ct/sourcedb_plugin/wikipedia_plugin.py
@@ -0,0 +1,20 @@
+import wikipedia
+
+class LessonDoc(object):
+    sourceDB = 'wikipedia'
+    def __init__(self, sourceID):
+        try:
+            self._data = wikipedia.page(sourceID)
+        except wikipedia.exceptions.DisambiguationError, e:
+            raise KeyError(str(e))
+        self.sourceID = sourceID
+        self.title = sourceID
+        self.description = self._data.summary
+        self.url = self._data.url
+
+    @classmethod
+    def search(klass, query, max_results=10):
+        'return list of [(title, sourceID, url)]'
+        return [(s, s, 'http://en.wikipedia.org/wiki/' + '_'.join(s.split()))
+                for s in wikipedia.search(query, max_results)]
+
