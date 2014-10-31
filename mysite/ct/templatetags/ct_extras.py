ee05d49a8e0d940b9d83f9887960997e34f4eddb
Christopher Lee <leec@chem.ucla.edu>
2014-10-31 11:51:51 -0700

    fixed temporary marker bug and added video directive support to md2html()

--- mysite/ct/templatetags/ct_extras.py
+++ mysite/ct/templatetags/ct_extras.py
@@ -15,9 +15,11 @@ StaticImagePat = re.compile(r'STATICIMAGE/([^"]+)')
 def md2html(txt, stripP=False):
     'converst ReST to HTML using pandoc, w/ audio support'
     txt, markers = add_temporary_markers(txt, find_audio)
+    txt, videoMarkers = add_temporary_markers(txt, find_video, len(markers))
     txt = pypandoc.convert(txt, 'html', format='rst',
                            extra_args=('--mathjax',))
     txt = replace_temporary_markers(txt, audio_html, markers)
+    txt = replace_temporary_markers(txt, video_html, videoMarkers)
     txt = StaticImagePat.sub(staticfiles.static('ct/') + r'\1', txt)
     if stripP and txt.startswith('<p>') and txt.endswith('</p>'):
         txt = txt[3:-4]
@@ -42,9 +44,14 @@ def find_audio(txt, lastpos, tag='.. audio::'):
         lastpos = i + len(tag)
         k = txt.find('\n', lastpos)
         if k < 0:
-            return -1, None, None
+            k = txt.find('\r', lastpos)
+        if k < 0: # no EOL, slurp to end of text
+            k = len(txt)
         v = txt[lastpos:k].strip()
         return i, k, v
+
+def find_video(txt, lastpos, tag='.. video::'):
+    return find_audio(txt, lastpos, tag)
 
 def audio_html(filename):
     i = filename.rfind('.')
@@ -53,18 +60,39 @@ def audio_html(filename):
     return '<audio controls><source src="STATICIMAGE/%s.ogg" type="audio/ogg"><source src="STATICIMAGE/%s.mp3" type="audio/mpeg">no support for audio!</audio>' \
       % (filename,filename)
     
-def add_temporary_markers(txt, func, l=None):
+def video_html(filename):
+    try:
+        sourceDB, sourceID = filename.split(':')
+    except ValueError:
+        return 'ERROR: bad video source: %s' % filename
+    d = {
+        'youtube': '''<iframe width="560" height="315"
+        src="http://www.youtube.com/embed/%s?rel=0"
+        frameborder="0" allowfullscreen></iframe>
+        ''',
+        'vimeo': '''<iframe width="500" height="281"
+        src="http://player.vimeo.com/video/%s"
+        frameborder="0" webkitallowfullscreen mozallowfullscreen
+        allowfullscreen></iframe>
+        ''',
+        }
+    try:
+        return d[sourceDB] % sourceID
+    except KeyError:
+        return 'ERROR: unknown video sourceDB: %s' % sourceDB
+
+def add_temporary_markers(txt, func, base=0, l=None):
     s = ''
     lastpos = 0
     if l is None:
         l = []
     while True: # replace selected content with unique markers
         i, j, v = func(txt, lastpos)
-        if i < 0: # missing terminator, so treat as regular text
+        if i < 0: # no more markers
             break
-        marker = 'mArKeR:%d:' % len(l)
+        marker = 'mArKeR:%d:' % (base + len(l))
         l.append((marker, v))
-        s += marker
+        s += txt[lastpos:i] + marker
         lastpos = j
     s += txt[lastpos:]
     return s, l
