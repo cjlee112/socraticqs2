4fb9161cf4f1b1376dc42a24fd364f978a4f934d
Christopher Lee <leec@chem.ucla.edu>
2014-12-14 15:40:07 -0800

    added simple Lesson test_creation_treeID

--- mysite/ct/tests.py
+++ mysite/ct/tests.py
@@ -5,12 +5,17 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+from ct.models import *
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class LessonMethodTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='jacob', email='jacob@_',
+                                             password='top_secret')
+    def test_creation_treeID(self):
+        'treeID properly initialized to default?'
+        lesson = Lesson(title='foo', text='bar', addedBy=self.user)
+        lesson.save_root()
+        l2 = Lesson.objects.get(pk=lesson.pk)
+        self.assertEqual(l2.treeID, l2.pk)
