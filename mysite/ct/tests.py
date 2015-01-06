21c617a03cc37ab61be56e48c742155c0d6eb79d
Christopher Lee <leec@chem.ucla.edu>
2015-01-05 23:17:44 -0800

    added wikipedia tests

--- mysite/ct/tests.py
+++ mysite/ct/tests.py
@@ -9,6 +9,29 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from ct.models import *
 
+class ConceptMethodTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='jacob', email='jacob@_',
+                                             password='top_secret')
+        self.wikiUser = User.objects.create_user(username='wikipedia', email='wiki@_',
+                                             password='top_secret')
+    def test_sourceDB(self):
+        'check wikipedia concept retrieval'
+        c, lesson = Concept.get_from_sourceDB('New York City', self.user)
+        self.assertEqual(c.title, 'New York City')
+        self.assertEqual(c.addedBy, self.user)
+        self.assertEqual(lesson.addedBy, self.wikiUser)
+        self.assertEqual(lesson.concept, c)
+        self.assertEqual(lesson.sourceDB, 'wikipedia')
+        self.assertEqual(lesson.sourceID, 'New York City')
+        self.assertIn('City of New York', lesson.text)
+        # check that subsequent retrieval uses stored db record
+        c2, l2 = Concept.get_from_sourceDB('New York City', self.user)
+        self.assertEqual(c2.pk, c.pk)
+        self.assertEqual(l2.pk, lesson.pk)
+        self.assertIn(c, list(Concept.search_text('new york')))
+
+
 class LessonMethodTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='jacob', email='jacob@_',
@@ -19,3 +42,11 @@ class LessonMethodTests(TestCase):
         lesson.save_root()
         l2 = Lesson.objects.get(pk=lesson.pk)
         self.assertEqual(l2.treeID, l2.pk)
+
+    def test_search_sourceDB(self):
+        'check wikipedia search'
+        results = Lesson.search_sourceDB('new york city')
+        self.assertTrue(len(results) >= 10)
+        self.assertIn('New York City', [t[0] for t in results])
+        self.assertEqual(len(results[0]), 3)
+        
