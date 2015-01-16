b5fc64ed0e40b2935c5d71471132d08ac89722f9
Christopher Lee <leec@chem.ucla.edu>
2015-01-16 14:03:29 -0800

    added FSMSpecification class for writing FSM specifications

---
+++ mysite/ct/fsm_plugin/fsmspec.py
@@ -0,0 +1,30 @@
+from ct.models import FSM
+
+class FSMSpecification(object):
+    'convenience class for specifying an FSM graph, loading it'
+    def __init__(self, name, title, nodeDict, edges=None,
+                 pluginNodes=(), **kwargs):
+        kwargs['name'] = name
+        kwargs['title'] = title
+        self.fsmData = kwargs
+        if not edges:
+            edges = []
+        for node in pluginNodes: # expect list of node class objects
+            modName = node.__module__.split('.')[-1]
+            name = node.__name__
+            d = dict(title=node.title, funcName=modName + '.' + name)
+            d['description'] = getattr(node, '__doc__', None)
+            d['help'] = getattr(node, 'help', None)
+            d['path'] = getattr(node, 'path', None)
+            d['data'] = getattr(node, 'data', None)
+            nodeDict[name] = d
+            for e in node.edges:
+                e['fromNode'] = name
+                edges.append(e)
+        self.nodeData = nodeDict
+        self.edgeData = edges
+    def save_graph(self, *args, **kwargs):
+        'load this FSM specification into the database'
+        return FSM.save_graph(self.fsmData, self.nodeData, self.edgeData,
+                                   *args, **kwargs)
+            
