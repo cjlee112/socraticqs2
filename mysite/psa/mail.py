949a9990f6584364a07b8be228c4815ee9b55e9b
Maksim Sokolskiy <misokolsky@gmail.com>
2015-04-23 06:19:11 -0400

    fix(psa): fix email validation in different sessions
    Changes:
    - mysite/psa/mail.py: add '&email=' + code.email to send_validation func

--- mysite/psa/mail.py
+++ mysite/psa/mail.py
@@ -4,8 +4,9 @@ from django.core.urlresolvers import reverse
 
 
 def send_validation(strategy, backend, code):
-    url = reverse('social:complete', args=(backend.name,)) + \
-            '?verification_code=' + code.code
+    url = (reverse('social:complete', args=(backend.name,)) +
+           '?verification_code=' + code.code +
+           '&email=' + code.email)
     url = strategy.request.build_absolute_uri(url)
     send_mail('Validate your account',
               'Validate your account {0}'.format(url),
