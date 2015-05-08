df92f4bd22048e719812ff3a27ebaf1e381132fa
Christopher Lee <leec@chem.ucla.edu>
2015-05-08 15:13:21 -0700

    improved auto switching between production vs. developer environment

--- mysite/mysite/settings/__init__.py
+++ mysite/mysite/settings/__init__.py
@@ -1,1 +1,4 @@
-from developer_conf import *
+try: # assume we are on production environment
+    from production_conf import *
+except ImportError: # assume we are on developer environment
+    from developer_conf import *
