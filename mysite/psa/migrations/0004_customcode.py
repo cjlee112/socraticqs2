4d8a0295312b3b4af0c2cc7febab9bd7b41fe258
Maksim Sokolskiy <misokolsky@gmail.com>
2015-05-12 18:43:50 +0300

    feat(psa): add ability to confirm email from different PC (cherry picked from commit 7fb8a02)

---
+++ mysite/psa/migrations/0004_customcode.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('default', '0003_alter_email_max_length'),
+        ('psa', '0003_auto_20150420_0308'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CustomCode',
+            fields=[
+                ('code_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='default.Code')),
+                ('user_id', models.IntegerField(null=True)),
+            ],
+            options={
+            },
+            bases=('default.code',),
+        ),
+    ]
--- mysite/psa/tests.py
+++ mysite/psa/tests.py
@@ -4,6 +4,17 @@ from django.test.client import RequestFactory
 from django.contrib.auth.models import User, AnonymousUser
 from django.http import HttpResponse, HttpRequest
 
+# from social.apps.django_app.default.models import UserSocialAuth
+#
+# from ct.models import (Role,
+#                        Course,
+#                        Unit,
+#                        UnitLesson,
+#                        UnitStatus,
+#                        FSMState,
+#                        FSMNode,
+#                        Respose,
+#                        StudentError)
 from psa.views import (context,
                        validation_sent,
                        custom_login,
@@ -117,3 +128,48 @@ class ViewsUnitTest(TestCase):
         self.assertTrue('Your password was changed' in response.content)
         user = User.objects.get(username='test')
         self.assertTrue(user.check_password('test2'))
+
+
+# class PipelineTest(TestCase):
+#     """Testing of pipeline.py"""
+#
+#     def setUp(self):
+#         self.user = User(username='test')
+#         self.user.save()
+#         self.course = Course(title='test title',
+#                              description='test description',
+#                              access='Public',
+#                              addedBy=self.user)
+#         self.course.save()
+#         self.unit1 = Unit(title='testunit1', kind='Courselet',
+#                           addedBy=self.user)
+#         self.unit2 = Unit(title='testunit2', kind='Courselet',
+#                           addedBy=self.user)
+#
+#     def test_social_merge(self):
+#         user1 = User(username='first')
+#         user1.save()
+#         user2 = User(username='second')
+#         user2.save()
+#
+#         role1 = Role(role='self', course=self.course, user=user1)
+#         role2 = Role(role='self', course=self.course, user=user2)
+#         role1.save()
+#         role2.save()
+#
+#         unitstatus1 = UnitStatus(unit=self.unit1, user=user1)
+#         unitstatus2 = UnitStatus(unit=self.unit2, user=user2)
+#         unitstatus1.save()
+#         unitstatus2.save()
+#
+#         unitLesson1 = UnitLesson(unit=self.unit1,
+#                                  kind='Answer for a question',
+#                                  addedBy=user1)
+#         unitLesson1.save()
+#         unitLesson2 = UnitLesson(unit=self.unit2,
+#                                  kind='Answer for a question',
+#                                  addedBy=user2)
+#         unitLesson2.save()
+
+
+
