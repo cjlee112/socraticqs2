89261b8680f70dc328164717dd99fc04ffad69f9
Maksim Sokolskiy <misokolsky@gmail.com>
2015-05-13 11:39:46 +0300

    ref(all): add docstrings (cherry picked from commit c38f381)

--- mysite/psa/mail.py
+++ mysite/psa/mail.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 
 
 def send_validation(strategy, backend, code):
+    """Send email validation link"""
     # TODO add email validating regex [^@]+@[^@]+\.[^@]+
     url = (reverse('social:complete', args=(backend.name,)) +
            '?verification_code=' + code.code +
