29e0fbb670db9f2c95d1ca61908cb86eabdb9352
Maksim Sokolskiy <misokolsky@gmail.com>
2015-05-28 12:31:10 +0300

    ref(check-quality.sh): improve check-quality.sh, add pylintrc

--- check-quality.sh
+++ check-quality.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-VENV_PATH="./.env"
-PEP8_CONF_PATH="./config/pep8.conf"
+
+PEP8_CONF_PATH="./config/pep8rc"
+PYLINT_CONF_PATH="./config/pylintrc"
 LOGS_PATH="./logs"
 
 function run_pep8 {
 
-    $VENV_PATH/bin/pep8 --config=$PEP8_CONF_PATH mysite/lti > $LOGS_PATH/pep8.log
-    $VENV_PATH/bin/pep8 --config=$PEP8_CONF_PATH mysite/psa >> $LOGS_PATH/pep8.log
-    $VENV_PATH/bin/pep8 --config=$PEP8_CONF_PATH mysite/ct >> $LOGS_PATH/pep8.log
-    $VENV_PATH/bin/pep8 --config=$PEP8_CONF_PATH mysite/mysite >> $LOGS_PATH/pep8.log
+    pep8 --config=$PEP8_CONF_PATH mysite/lti > $LOGS_PATH/pep8.log
+    pep8 --config=$PEP8_CONF_PATH mysite/psa >> $LOGS_PATH/pep8.log
+    pep8 --config=$PEP8_CONF_PATH mysite/ct >> $LOGS_PATH/pep8.log
+    pep8 --config=$PEP8_CONF_PATH mysite/mysite >> $LOGS_PATH/pep8.log
     
     cat $LOGS_PATH/pep8.log
 
@@ -16,10 +17,10 @@ function run_pep8 {
 
 function run_pylint {
 
-    $VENV_PATH/bin/pylint --errors-only mysite/lti > $LOGS_PATH/pylint.log
-    $VENV_PATH/bin/pylint --errors-only mysite/psa >> $LOGS_PATH/pylint.log
-    $VENV_PATH/bin/pylint --errors-only mysite/ct >> $LOGS_PATH/pylint.log
-    $VENV_PATH/bin/pylint --errors-only mysite/mysite >> $LOGS_PATH/pylint.log
+    pylint --rcfile=$PYLINT_CONF_PATH mysite/lti > $LOGS_PATH/pylint.log
+    pylint --rcfile=$PYLINT_CONF_PATH mysite/psa >> $LOGS_PATH/pylint.log
+    pylint --rcfile=$PYLINT_CONF_PATH mysite/ct >> $LOGS_PATH/pylint.log
+    pylint --rcfile=$PYLINT_CONF_PATH mysite/mysite >> $LOGS_PATH/pylint.log
 
     cat $LOGS_PATH/pylint.log
 
