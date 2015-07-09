1223d789e777485ee4c06e6988e780b7a39b86ee
Maksim Sokolskiy <misokolsky@gmail.com>
2015-07-09 19:13:21 +0300

    docs(lti): update documentation for Course creation feature
    Changes:
    - lti/utils.py: add functools.wraps to allow sphinx to get docstrings from decorated functions
    - lti/utils.py: add docstring to decorator
    - doc/lti.rst: update LTI documentation to add new Course creation feature

--- mysite/lti/utils.py
+++ mysite/lti/utils.py
@@ -1,7 +1,13 @@
+from functools import wraps
+
 from django.http import HttpResponse
 
 
 def only_lti(fn):
+    """
+    Decorator ensures that user comes from LTI session.
+    """
+    @wraps(fn)
     def wrapped(request, *args, **kwargs):
         try:
             request.session['LTI_POST']
