65dfe8e3edcd370d42cc140274a2b1030af04b5f
Maksim Sokolskiy <misokolsky@gmail.com>
2015-07-23 11:58:48 +0300

    ref(fsm): add db indexes to fields by which filtering are performed
    Rationalize:
    - FSMGroup.group: index added due to `FSM.objects.filter(fsmgroup__group=groupName)` in ct/views.py:203
    - FSMNode.name: index added due to `self.fsmnode_set.get(name=name)` in fsm/models.py:219
    - FSMEdge.name: index added due to `self.fsmNode.outgoing.get(name=name)` in fsm/models.py:419 and `pageData.fsmStack.state.fsmNode.outgoing.filter(name=task)` in fsm/views.py:68

---
+++ mysite/fsm/migrations/0002_auto_20150723_0243.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fsm', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='fsmedge',
+            name='name',
+            field=models.CharField(max_length=64, db_index=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='fsmgroup',
+            name='group',
+            field=models.CharField(max_length=64, db_index=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='fsmnode',
+            name='name',
+            field=models.CharField(max_length=64, db_index=True),
+            preserve_default=True,
+        ),
+    ]
