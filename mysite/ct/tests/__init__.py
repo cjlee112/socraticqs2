1963f961ab44bdb54a2255e68943b4b821fe5cbc
Maksim Sokolskiy <misokolsky@gmail.com>
2015-09-21 14:17:25 +0300

    test(ct): remove selenium tests from main test suite
    To run selenium tests run `python manage.py test ct.tests.selenium_integrate`

--- mysite/ct/tests/__init__.py
+++ mysite/ct/tests/__init__.py
@@ -2,7 +2,6 @@ from integrate import *
 from views import *
 from models import *
 from forms import *
-from selenium_integrate import *
 from plugins import *
 from templatetags import *
 from ct_util import *
