ef735db0be6a2413c0716def3ce0c1679845430e
Maksim Sokolskiy <misokolsky@gmail.com>
2015-10-28 12:40:33 +0200

    ref(ct): refactor ModelAdmin creation process according to code review, fix typo
    Notes:
    - Not using modelform_factory(model, form=BaseForm, fields='__all__') due to the same code in get_form method

--- mysite/ct/admin.py
+++ mysite/ct/admin.py
@@ -16,86 +16,6 @@ class BaseForm(ModelForm):
                 self.initial[field_name] = self.current_user
 
 
-class ConceptForm(BaseForm):
-    """
-    Form for AdminConcept.
-    """
-    class Meta:
-        models = ct.models.Concept
-
-
-class ConceptGraphForm(BaseForm):
-    """
-    Form for AdminConceptGraph.
-    """
-    class Meta:
-        models = ct.models.ConceptGraph
-
-
-class LessonForm(BaseForm):
-    """
-    Form for AdminLesson.
-    """
-    class Meta:
-        models = ct.models.Lesson
-
-
-class ConceptLinkForm(BaseForm):
-    """
-    Form for AdminConceptLink.
-    """
-    class Meta:
-        models = ct.models.ConceptLink
-
-
-class UnitLessonForm(BaseForm):
-    """
-    Form for AdminUnitLesson.
-    """
-    class Meta:
-        models = ct.models.UnitLesson
-
-
-class UnitForm(BaseForm):
-    """
-    Form for AdminUnit.
-    """
-    class Meta:
-        models = ct.models.Unit
-
-
-class ResponseForm(BaseForm):
-    """
-    Form for AdminResponse.
-    """
-    class Meta:
-        models = ct.models.Response
-
-
-class StudentErrorForm(BaseForm):
-    """
-    Form for AdminStudentError.
-    """
-    class Meta:
-        models = ct.models.StudentError
-
-
-class CourseForm(BaseForm):
-    """
-    Form for AdminCourse.
-    """
-    class Meta:
-        models = ct.models.Course
-
-
-class CourseUnitForm(BaseForm):
-    """
-    Form for AdminCourseUnit.
-    """
-    class Meta:
-        models = ct.models.CourseUnit
-
-
 class BaseAdmin(admin.ModelAdmin):
     """
     Base class for admin models.
@@ -105,55 +25,23 @@ class BaseAdmin(admin.ModelAdmin):
         form.current_user = request.user
         return form
 
-
-@admin.register(ct.models.Concept)
-class AdminConcept(BaseAdmin):
-    form = ConceptForm
+    form = BaseForm
 
 
-@admin.register(ct.models.ConceptGraph)
-class AdminConceptGraph(BaseAdmin):
-    form = ConceptGraphForm
+def register_model(model):
+    @admin.register(model)
+    class AdminModel(BaseAdmin):
+        pass
 
 
-@admin.register(ct.models.Lesson)
-class AdminLesson(BaseAdmin):
-    form = LessonForm
+models = [
+    ct.models.Concept, ct.models.ConceptGraph, ct.models.Lesson,
+    ct.models.ConceptLink, ct.models.UnitLesson, ct.models.Unit,
+    ct.models.Response, ct.models.StudentError, ct.models.Course,
+    ct.models.CourseUnit
+]
 
-
-@admin.register(ct.models.ConceptLink)
-class AdminConceptLink(BaseAdmin):
-    form = ConceptLinkForm
-
-
-@admin.register(ct.models.UnitLesson)
-class AdminUnitLesson(BaseAdmin):
-    form = UnitLessonForm
-
-
-@admin.register(ct.models.Unit)
-class AdminUnit(BaseAdmin):
-    form = UnitForm
-
-
-@admin.register(ct.models.Response)
-class AdminResponse(BaseAdmin):
-    form = ResponseForm
-
-
-@admin.register(ct.models.StudentError)
-class AdminStudentError(BaseAdmin):
-    form = StudentErrorForm
-
-
-@admin.register(ct.models.Course)
-class AdminCourse(BaseAdmin):
-    form = CourseForm
-
-
-@admin.register(ct.models.CourseUnit)
-class AdminCourseUnit(BaseAdmin):
-    form = CourseUnitForm
+map(register_model, models)
 
 
 @admin.register(ct.models.Role)
