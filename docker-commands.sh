499958c9e58afea18db75adb86518248fa867bd2
Maksim Sokolskiy <misokolsky@gmail.com>
2015-11-19 21:47:32 +0200

    fix(docker): fix docker_conf.py for TravisCI - rename

--- docker-commands.sh
+++ docker-commands.sh
@@ -8,8 +8,8 @@ echo "Done."
 if [ -f /code/mysite/mysite/settings/local_conf.py ] ; then
   echo "!!!!!!!!!!!!!!!!!!!!!!\nThere is existing local_conf.py file and we do not replace it now.\nCheck DATABASE settings.\ndocker_conf.py is example for local_conf.py"
 else
-  echo "Copy docker_conf.py to local_conf.py"
-  cp /code/mysite/mysite/settings/docker_conf.py /code/mysite/mysite/settings/local_conf.py
+  echo "Copy docker_conf.py_example to local_conf.py"
+  cp /code/mysite/mysite/settings/docker_conf.py_example /code/mysite/mysite/settings/local_conf.py
   echo "Done."
 fi
 
