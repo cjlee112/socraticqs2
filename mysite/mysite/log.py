70b70c3bea4a1b03843498722451f63939cde114
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2015-11-27 16:52:21 +0200

    Fix log message bug

--- mysite/mysite/log.py
+++ mysite/mysite/log.py
@@ -11,9 +11,7 @@ full_path = os.path.dirname(__file__) + '/../../logs/'+filename
 
 
 def write_exception_to_log(exception):
-    message = ''
-    if exception.message:
-        message = '- Exception.message [%s]' % exception.message
+    message =  exception.message if exception.message else str(exception)
     with open(full_path, "a") as log_file:
-        log_file.write("Time [%s] - Exception class [%s] %s \n" %
+        log_file.write("Time [%s] - Exception class [%s] - Exception message [%s] \n" %
                        (datetime.now(), exception.__class__, message))
