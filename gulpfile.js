0618697acca38c5c8b1abb92e6365cc8f5901cad
Dmitry <derdmitry@gmail.com>
2015-12-04 18:45:11 +0200

    Fix path add package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,43 @@
+function Logging(){
+  var stream = through.obj(function (file, enc, callback) {
+    console.log("stream:");
+    console.log(file, enc, callback);
+    this.push(file);
+    return callback();
+  });
+
+  return stream;
+}
+
 var gulp = require('gulp'),
     stylus = require('gulp-stylus'), 
     csso = require('gulp-csso'), 
     uglify = require('gulp-uglify'), 
     concat = require('gulp-concat'),
     minifyCSS = require('gulp-minify-css'),
+    debug = require('gulp-debug'),
     autoprefixer = require('gulp-autoprefixer');
+var through = require('through2');
 
-gulp.task('default', function() {
-    gulp.run("js");
-    gulp.run("css")
 
-});
+
 //  JS
 gulp.task('js', function() {
-    gulp.src(['./mysite/mysite/static/js/*.js', ])
-        .pipe(concat('main..min.js')) 
-        .pipe(gulp.dest('./public/js'))
-        .pipe(uglify());
+   return gulp.src(['./mysite/assets/js/src/*.js', ])
+       .pipe(concat('main.min.js'))
+       .pipe(uglify())
+       .on('error', console.log)
+       //.pipe(debug())
+       .pipe(gulp.dest('./mysite/mysite/static/js/'));
+
 });
 // CSS
 gulp.task('css', function() {
-    gulp.src(['./mysite/mysite/static/css/*.css', ])
+    return gulp.src(['./mysite/assets/css/src/*.css', ])
         .pipe(minifyCSS())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-        .pipe(concat('main.css')) 
-        .pipe(gulp.dest('./public/css'));
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('./mysite/mysite/static/css/'));
 });
+
+gulp.task('default', ['js', 'css']);
