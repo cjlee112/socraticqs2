1643776e3c0fba93474512be7e2807e30d4b6748
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2015-12-08 17:39:20 +0200

    Minor bug fixes

--- mysite/ct/views.py
+++ mysite/ct/views.py
@@ -789,7 +789,8 @@ def _lessons(request, pageData, concept=None, msg='',
             s = searchForm.cleaned_data['search']
             if searchType is None:
                 searchType = searchForm.cleaned_data['searchType']
-            lessonSet = UnitLesson.search_text(s, searchType, dedupe=False).exclude(unit=unit)
+            lessonSet = UnitLesson.search_text(s, searchType, dedupe=False)\
+                # .exclude(unit=unit)
             treeIDs_head = distinct_subset(lessonSet)
             branches = lessonSet.exclude(id__in=[each.id for each in treeIDs_head])
             tree_dict = {head_lesson: list(branches.filter(treeID=head_lesson.treeID)) for head_lesson in treeIDs_head}
