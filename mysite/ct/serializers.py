23cec4b656d5a1275cb071c1c9774193f6baf694
Maksim Sokolskiy <misokolsky@gmail.com>
2015-12-10 15:33:22 +0200

    fix(ct): fix fields naming

--- mysite/ct/serializers.py
+++ mysite/ct/serializers.py
@@ -4,32 +4,32 @@ from ct.models import Response
 
 
 class ResponseSerializer(serializers.HyperlinkedModelSerializer):
-    get_author_id = serializers.SerializerMethodField('author_id')
-    get_lti_identity = serializers.SerializerMethodField('lti_identity')
-    get_unitLesson_id = serializers.SerializerMethodField('unitLesson_id')
-    get_unit_id = serializers.SerializerMethodField('unit_id')
+    author_id = serializers.SerializerMethodField()
+    lti_identity = serializers.SerializerMethodField()
+    unitLesson_id = serializers.SerializerMethodField()
+    unit_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Response
         fields = (
             'id',
-            'get_author_id',
-            'get_lti_identity',
+            'author_id',
+            'lti_identity',
             'text',
             'confidence',
             'selfeval',
             'status',
-            'get_unitLesson_id',
-            'get_unit_id'
+            'unitLesson_id',
+            'unit_id',
         )
 
-    def author_id(self, obj):
+    def get_author_id(self, obj):
         """
         Returning author id.
         """
         return obj.author.id
 
-    def lti_identity(self, obj):
+    def get_lti_identity(self, obj):
         """
         Returning LTI user_id.
         """
@@ -37,13 +37,13 @@ class ResponseSerializer(serializers.HyperlinkedModelSerializer):
         if lti_user:
             return lti_user.user_id
 
-    def unitLesson_id(self, obj):
+    def get_unitLesson_id(self, obj):
         """
         Returning UnitLesson id.
         """
         return obj.unitLesson.id
 
-    def unit_id(self, obj):
+    def get_unit_id(self, obj):
         """
         Returning Unit id.
         """
