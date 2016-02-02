7298bed188f2e3be245e0d11bbd93010797a0b3e
Maksim Sokolskiy <misokolsky@gmail.com>
2016-02-02 21:37:18 +0200

    feat(ct:api): add ErrorViewSet to return StudentError based on course_id, also include ErrorModel data into StudentError report

--- mysite/ct/api.py
+++ mysite/ct/api.py
@@ -1,13 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAdminUser
 
-from ct.serializers import ResponseSerializer
-from ct.models import Response
+from ct.serializers import ResponseSerializer, ErrorSerializer
+from ct.models import Response, StudentError
 
 
 class ResponseViewSet(viewsets.mixins.ListModelMixin, viewsets.GenericViewSet):
     """
-    Django RestFramework class to implement Course report.
+    Django RestFramework class to implement Course student responses report.
     """
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAdminUser,)
     queryset = Response.objects.filter(kind='orct', unitLesson__order__isnull=False)
     serializer_class = ResponseSerializer
 
@@ -16,4 +20,21 @@ class ResponseViewSet(viewsets.mixins.ListModelMixin, viewsets.GenericViewSet):
         course_id = self.kwargs.get('course_id')
         if course_id:
             queryset = queryset.filter(course__id=course_id)
+        return queryset
+
+
+class ErrorViewSet(viewsets.mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    Django RestFramework class to implement Course student errors report.
+    """
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAdminUser,)
+    queryset = StudentError.objects.all()
+    serializer_class = ErrorSerializer
+
+    def get_queryset(self):
+        queryset = super(ErrorViewSet, self).get_queryset()
+        course_id = self.kwargs.get('course_id')
+        if course_id:
+            queryset = queryset.filter(response__course__id=course_id)
         return queryset
