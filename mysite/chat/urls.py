65ae28cce1d3fe00cd548bc31fa8deb13173d341
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-03-23 12:50:07 +0200

    Add template view to urls

--- mysite/chat/urls.py
+++ mysite/chat/urls.py
@@ -12,29 +12,30 @@ inj = injections.Container()
 inj['next_handler'] = FsmHandler()
 MessagesViewFSM = inj.inject(MessagesView)
 ChatInitialViewFSM = inj.inject(ChatInitialView)
-#
-# inj_alternative = injections.Container()
-# inj_alternative['next_handler'] = SequenceHandler()
-# MessagesViewAlternative = inj_alternative.inject(MessagesView)
-# ChatInitialViewAlternative = inj_alternative.inject(ChatInitialView)
+
+inj_alternative = injections.Container()
+inj_alternative['next_handler'] = SequenceHandler()
+MessagesViewAlternative = inj_alternative.inject(MessagesView)
+ChatInitialViewAlternative = inj_alternative.inject(ChatInitialView)
 
 
 router = SimpleRouter()
 router.register(r'messages', MessagesViewFSM, base_name='messages')
-# router.register(r'alternative/messages', MessagesViewAlternative, base_name='messages')
+router.register(r'alternative/messages', MessagesViewAlternative, base_name='messages')
 
 urlpatterns = patterns(
     '',
+    url(r'^ui/$', TemplateView.as_view(template_name="cui/src/index.html")),
     url(
         r'^enrollcode/(?P<enroll_key>[a-zA-Z0-9]+)/$',
         ChatInitialViewFSM.as_view(),
         name='chat_enroll'
     ),
-    # url(
-    #     r'^alternative/enrollcode/(?P<enroll_key>[a-zA-Z0-9]+)/$',
-    #     ChatInitialViewAlternative.as_view(),
-    #     name='chat_enroll'
-    # ),
+    url(
+        r'^alternative/enrollcode/(?P<enroll_key>[a-zA-Z0-9]+)/$',
+        ChatInitialViewAlternative.as_view(),
+        name='chat_enroll'
+    ),
     url(r'^history/$', HistoryView.as_view(), name='history'),
     url(r'^progress/$', ProgressView.as_view(), name='progress'),
     url(r'^', include(router.urls)),
