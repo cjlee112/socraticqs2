8e51d853bbcdc158785a482e4ebb47d1758db0ca
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-03-24 17:38:52 +0200

    feat(chat): add js code for adding csrf to post req	uest

--- mysite/chat/static/js/app.js
+++ mysite/chat/static/js/app.js
@@ -38,6 +38,30 @@ $(window).on('load', function(){
   var $course = $('.chat-introduction header p');
   var $about = $('.chat-introduction-body');
 
+  $.ajaxSetup({
+     beforeSend: function(xhr, settings) {
+         function getCookie(name) {
+             var cookieValue = null;
+             if (document.cookie && document.cookie != '') {
+                 var cookies = document.cookie.split(';');
+                 for (var i = 0; i < cookies.length; i++) {
+                     var cookie = jQuery.trim(cookies[i]);
+                     // Does this cookie string begin with the name we want?
+                     if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                         break;
+                     }
+                 }
+             }
+             return cookieValue;
+         }
+         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+             // Only send the token to relative URLs i.e. locally.
+             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+         }
+     }
+  });
+
   // Hide spinner
   timeline.to($loading, 1, {opacity: 0, display: 'none', onComplete: function(){
     // Kill spinner animation timeline
