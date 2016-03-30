2281b59a987922ba462491f44e9666e81eebe257
Maksim Sokolskiy <misokolsky@gmail.com>
2016-03-30 17:39:22 +0300

    feat(chat): pass data from course and unit to `main_view.html` template

--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -1,12 +1,13 @@
 import injections
 from django.views.generic import View
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from .models import Chat, EnrollUnitCode
 from .services import ProgressHandler
-from ct.models import Unit, Role
+from ct.models import Unit, Role, UnitLesson
 
 
 @injections.has
@@ -43,14 +44,29 @@ class ChatInitialView(View):
             next_point = self.next_handler.start_point(unit=unit, chat=chat, request=request)
         else:
             next_point = chat.next_point
-        return render(request, 'cui/index.html')
-        # return render(
-        #     request,
-        #     'chat/main_view.html',
-        #     {
-        #         'chat_id': chat.id,
-        #         'lessons': unit.get_exercises(),
-        #         'next_point': next_point,
-        #         'fsmstate': chat.state
-        #     }
-        # )
+
+        lessons = unit.get_exercises()
+
+        concepts = []
+        for ul in unit.unitlesson_set.filter(lesson__concept__isnull=False, kind=UnitLesson.COMPONENT):
+            title = ul.lesson.concept.title
+            url = ul.lesson.url if ul.lesson.url else reverse(
+                'ct:study_concept', args=(courseUnit.course.id, unit.id, ul.id)
+            )
+            concepts.append((title, url))
+
+        return render(
+            request,
+            'chat/main_view.html',
+            {
+                'course': courseUnit.course,
+                'unit': unit,
+                'concepts': concepts,
+                'chat_id': chat.id,
+                'lessons': lessons,
+                'lesson_cnt': len(lessons),
+                'duration': len(lessons) * 3,
+                'next_point': next_point,
+                'fsmstate': chat.state
+            }
+        )
