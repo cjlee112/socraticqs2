6f50d082da421e078da2df896eb7fa21756568e7
Maksim Sokolskiy <misokolsky@gmail.com>
2016-04-20 21:52:43 +0300

    ref(chat): replace all `migrations` w/ `initial` one

--- mysite/chat/migrations/0001_initial.py
+++ mysite/chat/migrations/0001_initial.py
@@ -2,16 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import datetime
-import chat.utils
+import django.db.models.deletion
+import django.utils.timezone
 from django.conf import settings
+import chat.utils
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ct', '0016_auto_20150626_0301'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('fsm', '0002_auto_20150723_0243'),
+        ('ct', '0018_unit_small_img_url'),
     ]
 
     operations = [
@@ -20,6 +22,19 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('is_open', models.BooleanField(default=False)),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+            ],
+            options={
+                'ordering': ['-timestamp'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ChatDivider',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.CharField(max_length=64)),
+                ('unitlesson', models.ForeignKey(to='ct.UnitLesson', null=True)),
             ],
             options={
             },
@@ -30,7 +45,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('enrollCode', models.CharField(default=chat.utils.enroll_generator, max_length=32)),
-                ('unit', models.ForeignKey(to='ct.Unit')),
+                ('courseUnit', models.ForeignKey(to='ct.CourseUnit')),
             ],
             options={
             },
@@ -40,10 +55,32 @@ class Migration(migrations.Migration):
             name='Message',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('timestamp', models.DateTimeField(default=datetime.datetime.now, null=True)),
-                ('contenttype', models.CharField(default=b'NoneType', max_length=16, null=True, choices=[(b'NoneType', b'NoneType'), (b'divider', b'divider'), (b'response', b'response'), (b'lesson', b'lesson')])),
+                ('text', models.TextField(null=True, blank=True)),
+                ('timestamp', models.DateTimeField(null=True)),
+                ('contenttype', models.CharField(default=b'NoneType', max_length=16, null=True, choices=[(b'NoneType', b'NoneType'), (b'chatdivider', b'chatdivider'), (b'response', b'response'), (b'unitlesson', b'unitlesson'), (b'uniterror', b'uniterror')])),
                 ('content_id', models.IntegerField(null=True)),
-                ('chat', models.OneToOneField(null=True, to='chat.Chat')),
+                ('input_type', models.CharField(default=b'options', max_length=16, null=True, choices=[(b'text', b'text'), (b'options', b'options'), (b'custom', b'custom')])),
+                ('type', models.CharField(default=b'message', max_length=16, choices=[(b'message', b'message'), (b'user', b'user'), (b'breakpoint', b'breakpoint')])),
+                ('is_additional', models.BooleanField(default=False)),
+                ('kind', models.CharField(max_length=32, null=True, choices=[(b'base', b'base'), (b'orct', b'orct'), (b'answer', b'answer'), (b'errmod', b'errmod'), (b'chatdivider', b'chatdivider'), (b'uniterror', b'uniterror'), (b'response', b'response'), (b'message', b'message'), (b'button', b'button')])),
+                ('userMessage', models.BooleanField(default=False)),
+                ('chat', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='chat.Chat', null=True)),
+                ('lesson_to_answer', models.ForeignKey(to='ct.UnitLesson', null=True)),
+                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
+                ('response_to_check', models.ForeignKey(to='ct.Response', null=True)),
+                ('student_error', models.ForeignKey(to='ct.StudentError', null=True)),
+            ],
+            options={
+                'ordering': ['timestamp'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UnitError',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('response', models.ForeignKey(to='ct.Response')),
+                ('unit', models.ForeignKey(to='ct.Unit')),
             ],
             options={
             },
@@ -51,18 +88,36 @@ class Migration(migrations.Migration):
         ),
         migrations.AlterUniqueTogether(
             name='enrollunitcode',
-            unique_together=set([('enrollCode', 'unit')]),
+            unique_together=set([('enrollCode', 'courseUnit')]),
         ),
         migrations.AddField(
             model_name='chat',
-            name='next_step',
+            name='enroll_code',
+            field=models.ForeignKey(to='chat.EnrollUnitCode', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='chat',
+            name='instructor',
+            field=models.ForeignKey(related_name='course_instructor', blank=True, to=settings.AUTH_USER_MODEL, null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='chat',
+            name='next_point',
             field=models.OneToOneField(related_name='base_chat', null=True, to='chat.Message'),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='chat',
+            name='state',
+            field=models.OneToOneField(null=True, to='fsm.FSMState'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='chat',
             name='user',
-            field=models.OneToOneField(to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
             preserve_default=True,
         ),
     ]
