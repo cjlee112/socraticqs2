ad9924194fe92473601fc72e5129c386cb079523
Maksim Sokolskiy <misokolsky@gmail.com>
2016-04-27 13:21:28 +0300

    test(ct): add py.test integration

---
+++ mysite/ct/tests/test_integrate.py
@@ -0,0 +1,14 @@
+
+"""
+py.test tests for our Djngo application.
+"""
+
+
+def test_with_client(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_with_admin_client(admin_client):
+    response = admin_client.get('/admin/')
+    assert response.status_code == 200
