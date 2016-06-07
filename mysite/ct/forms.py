327fb11b14ccee5b0c9565501a695f2674fb2a30
Maksim Sokolskiy <misokolsky@gmail.com>
2016-06-07 12:35:50 +0300

    fix(ct): fix issue #146 Long descriptions
    Changes:
    - add `max_length` to `description` field in `CourseTitleForm`

--- mysite/ct/forms.py
+++ mysite/ct/forms.py
@@ -173,16 +173,20 @@ class NewUnitTitleForm(UnitTitleForm):
 
 
 class CourseTitleForm(forms.ModelForm):
+    description = forms.CharField(max_length=1500, widget=forms.widgets.Textarea)
     submitLabel = 'Update'
+
     def __init__(self, *args, **kwargs):
         super(CourseTitleForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_id = 'id-courseTitleForm'
         self.helper.form_class = 'form-vertical'
         self.helper.add_input(Submit('submit', self.submitLabel))
+
     class Meta:
         model = Course
         fields = ['title', 'access', 'description', 'trial']
+
 
 
 class NewCourseTitleForm(CourseTitleForm):
