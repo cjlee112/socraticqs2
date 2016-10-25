4acc0dee505c21cf45a7f44e24d0d12782d46201
Alex Boiko <oleksandr.boiko@raccoongang.com>
2016-10-25 16:29:16 +0300

    feature(lms): Fix in course_view test, added negative test, fix in template.
    Added negative test for course_view page, fix in template so now button Join will not be shown if there's no opened live session for course.

--- mysite/lms/tests.py
+++ mysite/lms/tests.py
@@ -15,8 +15,10 @@ class TestCourseView(TestCase):
     @patch('fsm.models.FSMState.find_live_sessions')
     def test_course_view(self, find_live_sessions, get_obj_or_404):
         """
-        This test tests that FSMState.find_live_sessions(request.user).filter(activity__course=course).first()
-        return course and this course is present in page's  context"""
+        This test tests that:
+         - query FSMState.find_live_sessions(request.user).filter(activity__course=course).first()
+           return live session and this live session is present in page's context
+        """
         
         filter_mock = Mock()
         filter_mock.filter = Mock()
@@ -33,8 +35,6 @@ class TestCourseView(TestCase):
         course_units.return_value = [Mock()]
         get_obj_or_404.return_value = course_mock
 
-
-
         response = self.client.get(reverse('lms:course_view', kwargs={'course_id': 1}))
 
         self.assertEqual(response.status_code, 200)
@@ -47,6 +47,34 @@ class TestCourseView(TestCase):
         self.assertIn('course', response.context)
         self.assertIn('liveSession', response.context)
         self.assertIn('courslets', response.context)
+
+
+    def test_course_view_negative(self):
+        """
+        This test tests case when teacher not yet (opened) joined live session and
+        student opens course page.
+        Student should not see 'Join Live Session' button on the top of the page.
+        """
+        self.course = Course(
+            title='Great Course', description='the bestest', addedBy=self.user
+        )
+        self.course.save()
+
+        response = self.client.get(reverse('lms:course_view', kwargs={'course_id': self.course.id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lms/course_page.html')
+        self.assertIn('course', response.context)
+        self.assertIn('liveSession', response.context)
+        self.assertIn('courslets', response.context)
+        self.assertEqual(response.context['liveSession'], None)
+        self.assertFalse(response.context['liveSession'])
+
+    #TODO: write test when teacher really creates Course and Courslets inside of the course and student open page.
+    #TODO: user should see 'Join' button.
+
+
+
 
 
 
