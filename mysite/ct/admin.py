9420aaac79778995d8e8d55167e0f9e178d5ac6b
Alex Boiko <django.develop@gmail.com>
2016-10-27 19:02:21 +0300

    fix(ct.admin): Added Liked model into admin UI.
    Adds Liked model to admin UI, adds filter by username and lesson name.

--- mysite/ct/admin.py
+++ mysite/ct/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from ct.models import Liked
 
 from .forms import BaseForm
 from .models import (
@@ -12,7 +13,8 @@ from .models import (
     StudentError,
     Course,
     CourseUnit,
-    Role
+    Role,
+    Liked
 )
 
 
@@ -50,3 +52,17 @@ class AdminModel(BaseAdmin):
 @admin.register(Role)
 class AdminRole(admin.ModelAdmin):
     list_display = ('role', 'course', 'user')
+
+def user_username(obj):
+    return obj.addedBy.username
+user_username.short_description = 'Username'
+
+def lesson_title(obj):
+    return obj.unitLesson.lesson.title
+lesson_title.short_description = 'Lesson title'
+
+@admin.register(Liked)
+class AdminLiked(admin.ModelAdmin):
+
+    list_display = (user_username, lesson_title, 'atime')
+    list_filter = ('addedBy__username', 'unitLesson__lesson__title')
