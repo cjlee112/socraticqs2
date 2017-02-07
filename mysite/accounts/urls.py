4278f376be6c15ded3505e0934123424bc57711b
Alex Boiko <django.develop@gmail.com>
2017-02-07 15:46:14 +0200

    feat(issue#294): Updates buttons on account settings page

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns(
 
     # url(r'^password_change/$', password_change, {'post_change_redirect': 'accounts:password_change_done'}, name='password_change'),
     # url(r'^password_change/done/$', password_change_done, name='password_change_done'),
-    
+
     url(r'^password_reset/$', password_reset, {'post_reset_redirect': 'accounts:password_reset_done'}, name='password_reset'),
     url(r'^password_reset/done/$', password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
