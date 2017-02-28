3ab98b31a702774a8f537742e8f665edd46419e4
Maksim Sokolskiy <misokolsky@gmail.com>
2017-02-28 18:05:55 +0200

    fix(ctms): add `NewLoginRequiredMixin`

--- mysite/mysite/mixins.py
+++ mysite/mysite/mixins.py
@@ -1,14 +1,22 @@
-
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse_lazy
 from django.utils.decorators import method_decorator
 from mysite.decorators import not_anonymous_required
 
 
+class NewLoginRequiredMixin(object):
+    """
+    Ensures that user must be authenticated in order to access view.
+    """
+    @method_decorator(login_required(login_url='new_login'))
+    def dispatch(self, *args, **kwargs):
+        return super(NewLoginRequiredMixin, self).dispatch(*args, **kwargs)
+
+
 class LoginRequiredMixin(object):
-
-    u"""Ensures that user must be authenticated in order to access view."""
-
+    """
+    Ensures that user must be authenticated in order to access view.
+    """
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
