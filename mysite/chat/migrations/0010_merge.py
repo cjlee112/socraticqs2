2315efa7d6322ba1e801477a53fe08dfcfb370fb
Alex Boiko <django.develop@gmail.com>
2017-04-06 15:33:12 +0300

    Added merge migrations

---
+++ mysite/chat/migrations/0010_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('chat', '0007_chat_is_test'),
+        ('chat', '0009_enrollunitcode_ispreview'),
+    ]
+
+    operations = [
+    ]
