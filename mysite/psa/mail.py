7dcce29071d5774a5f03618d12d88a027b9cca0d
Alex Boiko <django.develop@gmail.com>
2017-06-02 17:29:51 +0300

    fix(emails): Added templates for emails (password reset, email confirmation).

--- mysite/psa/mail.py
+++ mysite/psa/mail.py
@@ -2,8 +2,10 @@
 Module defined send_validation function to verify emails.
 """
 from django.conf import settings
+from django.template import loader
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
+from django.template.context import Context
 
 
 def send_validation(strategy, backend, code):
@@ -14,9 +16,22 @@ def send_validation(strategy, backend, code):
     url = (reverse('social:complete', args=(backend.name,)) +
            '?verification_code=' + code.code)
     url = strategy.request.build_absolute_uri(url)
+    context = Context({
+        'code': code,
+        'url': url,
+    })
+
+    subj_template = loader.get_template('psa/email/confirmation_subject.txt')
+    rendered_subj = subj_template.render(context)
+
+    text_template = loader.get_template('psa/email/confirmation_text.txt')
+    rendered_text = text_template.render(context)
+
     send_mail(
-        'Validate your account',
-        'Validate your account {0}'.format(url),
+        rendered_subj,
+        rendered_text,
+        # 'Validate your account',
+        # 'Validate your account {0}'.format(url),
         settings.EMAIL_FROM,
         [code.email],
         fail_silently=False
