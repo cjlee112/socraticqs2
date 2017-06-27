3486ee1d6814f11f21052ac477129846ead4778e
Alex Boiko <django.develop@gmail.com>
2017-06-27 19:44:01 +0300

    fix(chat_init): Changes in chat initialization process.
    Now when user load page chat_enroll it will not create new chat. New chat will be created only after JS ajax request to api endpoint. This endpoint will create new chat and start FSM.
    
    fix(chat_init): Changes requested in PR
    
    fix(init_chat_js): Minor changes in chat initialization. Added more comments and docstrings.
    
    fix(chat_init_js): Fix tests for new init chat process

--- mysite/lms/tests.py
+++ mysite/lms/tests.py
@@ -120,3 +120,190 @@ class TestCourseView(TestCase):
         self.assertEqual(response.context['livesessions'][0].get_formatted_time_spent(), '1 day, 1:00:00')
 
 
+class TestCourseletViewHistoryTab(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='test')
+        self.client.login(username='test', password='test')
+
+        self.course = Course(title='test_title', addedBy=self.user)
+        self.course.save()
+        self.unit = Unit(title='test unit title', addedBy=self.user)
+        self.unit.save()
+        self.course_unit = CourseUnit(
+            course=self.course,
+            unit=self.unit,
+            order=0,
+            addedBy=self.user
+        )
+        self.course_unit.releaseTime = timezone.now() - datetime.timedelta(days=1)
+        self.course_unit.save()
+
+        self.enroll = EnrollUnitCode(courseUnit=self.course_unit)
+        self.enroll.save()
+
+        self.role = Role(course=self.course, user=self.user, role=Role.INSTRUCTOR)
+        self.role.save()
+
+        self.student_role = Role(course=self.course, user=self.user, role=Role.ENROLLED)
+        self.student_role.save()
+
+        self.concept = Concept.new_concept('bad', 'idea', self.unit, self.user)
+
+        self.lesson = Lesson(
+            title='New York Test Lesson',
+            text='brr',
+            addedBy=self.user,
+            kind=Lesson.ORCT_QUESTION
+        )
+        self.lesson.save_root(self.concept)
+
+        self.unit_lesson = UnitLesson(
+            unit=self.unit,
+            lesson=self.lesson,
+            addedBy=self.user,
+            treeID=self.lesson.id,
+            order=0
+        )
+        self.unit_lesson.save()
+
+        self.unit_lesson_answer = UnitLesson(
+            parent=self.unit_lesson,
+            unit=self.unit,
+            lesson=self.lesson,
+            addedBy=self.user,
+            treeID=self.lesson.id,
+            kind=UnitLesson.ANSWERS
+        )
+        self.unit_lesson_answer.save()
+
+        self.user = User.objects.create_user(username='admin', password='admin')
+
+        call_command('fsm_deploy')
+
+    def test_click_on_courslet_creates_new_chat(self):
+        # test that there's no history yet
+        response = self.client.get(
+            reverse('lms:course_view', kwargs={'course_id': self.course.id})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(list(response.context['courslets']))
+
+        self.assertEqual(response.status_code, 200)
+
+        chats_count_1 = Chat.objects.all().count()
+
+        # firstly call to chat:init_chat_api function with enroll_key and chat_id=0
+        response = self.client.get(
+            reverse(
+                'chat:init_chat_api',
+                kwargs={
+                    'enroll_key': self.enroll.enrollCode,
+                    'chat_id': 0
+                }
+            ),
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+        )
+        json_content = json.loads(response.content)
+        chat_id = json_content['id']
+
+        response = self.client.get(
+            reverse('chat:chat_enroll', kwargs={'enroll_key': self.enroll.enrollCode, 'chat_id': chat_id})
+        )
+        self.assertEqual(response.context['chat'].id, Chat.objects.all().first().id)
+        self.assertEqual(response.status_code, 200)
+        chats_count_2 = Chat.objects.count()
+
+        self.assertNotEqual(chats_count_2, chats_count_1)
+
+        response = self.client.get(
+            reverse('chat:chat_enroll', kwargs={'enroll_key': self.enroll.enrollCode, 'chat_id': chat_id})
+        )
+        chats_count_3 = Chat.objects.count()
+
+        response = self.client.get(
+            reverse('chat:chat_enroll', kwargs={'enroll_key': self.enroll.enrollCode, 'chat_id': chat_id})
+        )
+        chats_count_4 = Chat.objects.count()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(chats_count_4, chats_count_2)
+        self.assertEqual(chats_count_3, chats_count_2)
+
+        self.assertEqual(response.context['chat'].id, Chat.objects.all().first().id)
+
+        chat = Chat.objects.all().first()
+        # get chat and set state to None it means that courslet finished.
+        chat.state = None
+        chat.save()
+
+        response = self.client.get(
+            reverse('lms:course_view', kwargs={'course_id': self.course.id})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Chat.objects.count(), chats_count_2)
+        self.assertEqual(len(list(response.context['courslets'])), 1)
+
+    def test_courslet_history(self):
+        enroll_code = EnrollUnitCode.get_code(self.course_unit)
+
+        response = self.client.get(
+            reverse(
+                'chat:init_chat_api',
+                kwargs={
+                    'enroll_key': self.enroll.enrollCode,
+                    'chat_id': 0
+                }
+            ),
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+        )
+        json_content = json.loads(response.content)
+        chat_id = json_content['id']
+
+        response = self.client.get(
+            reverse('chat:chat_enroll', args=(enroll_code, chat_id)), follow=True
+        )
+
+        response = self.client.get(
+            reverse('chat:history'), {'chat_id': chat_id}, follow=True
+        )
+        json_content = json.loads(response.content)
+
+        next_url = json_content['input']['url']
+
+        answer = 'My Answer'
+        response = self.client.put(
+            next_url,
+            data=json.dumps({"text": answer, "chat_id": chat_id}),
+            content_type='application/json',
+            follow=True
+        )
+
+        json_content = json.loads(response.content)
+        next_url = json_content['input']['url']
+
+        response = self.client.get(
+            next_url, {'chat_id': chat_id}, follow=True
+        )
+
+        json_content = json.loads(response.content)
+        next_url = json_content['input']['url']
+
+        self.assertIsNotNone(json_content['input']['options'])
+        self.assertEquals(len(json_content['addMessages']), 2)
+
+        # emulate chat finished - set state to None
+
+        Chat.objects.filter(id=chat_id).update(state=None)
+
+        response = self.client.get(
+            reverse('chat:chat_enroll', args=(enroll_code, chat_id)), follow=True
+        )
+        response = self.client.get(
+            reverse('chat:history'), {'chat_id': chat_id}, follow=True
+        )
+        json_content = json.loads(response.content)
+
+        self.assertIsNone(json_content['input']['options'])
+        self.assertEquals(len(json_content['addMessages']), 4)
+
+
