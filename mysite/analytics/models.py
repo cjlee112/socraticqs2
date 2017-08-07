d04746df0fe2035f968757a00d525144ef47d756
Maksim Sokolskiy <misokolsky@gmail.com>
2017-08-07 12:31:57 +0300

    ref(analytics, Makefile): change docstring, add test for Makefile

--- mysite/analytics/models.py
+++ mysite/analytics/models.py
@@ -7,13 +7,17 @@ from filer.fields.file import FilerFileField
 from ct.models import Course
 
 def get_upload_function(folder=''):
-    '''
-    This function receive base path where to store files and return new function
-     which add user ID to this path.
-     We need it to personalize reports and store them in separate folders (folder named as user ID)
+    """
+    Path generation to store reports or errors.
+
+    This function receive base path where to store files and return
+    new function which add user ID to this path.
+    We need it to personalize reports and store them in separate
+    folders (folder named as user ID).
+
     :param folder: where to put report file
     :return: full path
-    '''
+    """
     def user_dir_path(instance, filename):
         user_id = str(instance.addedBy.id) if instance.addedBy else ''
         return os.path.join(folder, user_id, filename)
