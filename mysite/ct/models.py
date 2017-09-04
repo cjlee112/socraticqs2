cb364a9d2cc8bc629f2795e1758a4beb1fa6f6f5
Maksim Sokolskiy <misokolsky@gmail.com>
2017-09-04 13:22:08 +0300

    fix bad rebase, update .gitignore

--- mysite/ct/models.py
+++ mysite/ct/models.py
@@ -1106,7 +1106,9 @@ class Course(models.Model):
     def deep_clone(self, **options):
         publish = options.get('publish', False)
         with_students = options.get('with_students', False)
-        title = self.title.split('copied')[0] + " copied {}".format(timezone.now())
+        asis = options.get('asis', False)
+        title = self.title + " copied {}".format(timezone.now())
+
         new_course = copy_model_instance(
             self,
             atime=timezone.now(),
