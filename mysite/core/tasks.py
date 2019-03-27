c3838132d3a4622ab4c9660f574e8219ac5e164b
Maksim Sokolskiy <misokolsky@gmail.com>
2019-03-26 23:18:15 -0700

    Add logging for Intercom event generation

--- mysite/core/tasks.py
+++ mysite/core/tasks.py
@@ -1,16 +1,20 @@
+import logging
+
 from intercom.client import Client
 from django.conf import settings
 from celery import shared_task
 
 
+log = logging.getLogger(__name__)
 intercom = Client(personal_access_token=settings.INTERCOM_ACCESS_TOKEN)
 
 
 @shared_task
-def intercom_event(event_name, created_at, email, metadata):
+def intercom_event(event_name, created_at, email, metadata):    
     intercom.events.create(
         event_name=event_name,
         created_at=created_at,
         email=email,
         metadata=metadata
     )
+    log.info("{}:{}:{}:{}".format(event_name, created_at, email, metadata))
