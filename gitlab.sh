d400944d23a4f6c42eb0adc74970a18c1048d64f
Maksim Sokolskiy <misokolsky@gmail.com>
2019-06-11 16:20:56 +0300

    Improve increment delivery process
    Changes:
    - optimize usage of the previously user mongo container for pytest
    - add .migrate and .fsm-deploy steps for development deployment stage
    - use hard way to have a latest development branch on development instance
    
    TODO:
    - generate static files on build stage (or create new one for static) and use it in deploy stage

--- gitlab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-name='mongo'
-
-[[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] ||
-docker run -d --name mongo -p 27017:27017 mongo:latest
--- gitlab_stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-name='mongo'
-
-[[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] ||
-docker stop mongo
