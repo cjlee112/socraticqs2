e721326095eab520124cb7923658183df750f1ff
Sagirov Evgeniy <sagirov19@gmail.com>
2019-10-20 16:03:57 +0300

    Error appear when user click Next button on the Best practices tab for a Course

--- mysite/api/v0/utils.py
+++ mysite/api/v0/utils.py
@@ -10,12 +10,15 @@ def get_result_calculation(data, calculation) -> dict:
      - dict
      - empty dict in case data is empty
     """
+    def get_value_or_default(data, key, default):
+        value = data.get(key)
+        return value if value and value != '' else default
+
     result = {}
     if calculation and data:
-        result['w_o_courselets'] = int(
-            int(data.get('misconception_a_day', 5)) * 7)
+        result['w_o_courselets'] = int(get_value_or_default(data, 'misconception_a_day', 5)) * 7
         result['w_courselets'] = int(
-            int(data.get('bp_student_number', 200)) *
-            int(data.get('question_parts', 24)) *
-            ((1 - int(data.get('average_score', 72)) / 100)))
+            int(get_value_or_default(data, 'bp_student_number', 200)) *
+            int(get_value_or_default(data, 'question_parts', 24)) *
+            ((1 - int(get_value_or_default(data, 'average_score', 72)) / 100)))
     return result
