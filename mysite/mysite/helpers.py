017947e7457e79934316239ddf8bf4789bfab47e
Sagirov Evgeniy <sagirov19@gmail.com>
2019-10-21 11:56:02 +0300

    CR-424785 Instructor drawing a new Canvas answer overwrites other ORCT Canvas answers

--- mysite/mysite/helpers.py
+++ mysite/mysite/helpers.py
@@ -2,6 +2,8 @@ import base64
 
 from django.core.files.base import ContentFile
 
+from lti.utils import key_secret_generator
+
 
 def base64_to_file(data_attachment, prefix='image'):
     """
@@ -11,6 +13,6 @@ def base64_to_file(data_attachment, prefix='image'):
     if data_attachment and data_attachment.startswith('data:image'):
         format, image_string = data_attachment.split(';base64,')
         extension = format.split('/')[-1].split('+')[0]
-        name = '{}.{}'.format(prefix, extension)
+        name = '{}-{}.{}'.format(prefix, key_secret_generator(), extension)
         return ContentFile(base64.b64decode(image_string), name=name)
     return None
